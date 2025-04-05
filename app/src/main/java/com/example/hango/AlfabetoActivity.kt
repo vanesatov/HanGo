39727bea5c6bbbfefdc51c62a9dae3d10d0e45eb
vanesa <vanesatov@gmail,com>
2025-04-05 19:50:06 +0200

    pantalla Menu Principal y pantalla alfabetos creados

---
+++ app/src/main/java/com/example/hango/AlfabetoActivity.kt
@@ -0,0 +1,26 @@
+package com.example.hango
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.hango.databinding.ActivityAlfabetoBinding
+
+
+class AlfabetoActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAlfabetoBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityAlfabetoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/example/hango/MenuPrincipalActivity.kt
@@ -0,0 +1,33 @@
+package com.example.hango
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+import com.example.hango.databinding.ActivityMenuPrincipalBinding
+
+class MenuPrincipalActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMenuPrincipalBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMenuPrincipalBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Aquí enlazamos la tarjeta con el intent a AlfabetoActivity
+        binding.cardAlfabeto.setOnClickListener {
+            val intent = Intent(this, AlfabetoActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
